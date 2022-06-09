78bc55c1e96ac544b4196b3e6079c9d19ab144ec
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2022-06-09 15:21:36 +0100

    Added morgan logger for monitoring requests

--- app/startup/index.ts
+++ app/startup/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import logger from "morgan";
 import postRouter from "../routes/post.route";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
 app.use("/", postRouter);
 
 export default app;
