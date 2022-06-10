48664f1a955c52e25a93d75e1e81c5838f5dad9e
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2022-06-10 11:54:39 +0100

    Adding a default route for invalid endpoints

--- app/startup/index.ts
+++ app/startup/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Response } from "express";
 import logger from "morgan";
 import postRouter from "../routes/post.route";
 
@@ -8,5 +8,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
 app.use("/", postRouter);
+
+app.use("*", (_req, res: Response) => {
+  res.send({ message: "INVALID ROUTE" });
+});
 
 export default app;
